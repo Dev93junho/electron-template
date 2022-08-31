d7fcf8cd455516d4c5abd7e2867c074367b5948c
KBS <nordap@naver.com>
2022-08-31 17:21:19 +0900

    hello electron flask

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
-    let win = new BrowserWindow ({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-    win.loadFile('./index.html')
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.loadFile('./index.html')
+
+  let {PythonShell} = require('python-shell')
+  PythonShell.run('app.py', function (err, results) {
+    if (err) console.log(err);
+  });
 }
 
 app.on('ready', createWindow);
