39dc87e89b574067c45dfe2032e004697ff5e7d4
jhshin9345@gmail.com <jhshin9345@gmail.com>
2022-09-09 17:54:36 +0900

    updated predict and desc input

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import sys
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World from Flask"
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        description = request.args('desc')
+        # run onnx model
+    return # need to model outputs data type
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
